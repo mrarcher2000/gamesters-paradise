198a21ea2770640189f9e21ec015df5b8c61c5ea
Duartem795 <76462661+Duartem795@users.noreply.github.com>
2021-05-04 21:49:31 -0700

    finished a thing

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -9,6 +9,46 @@ router.get('/', (req, res) => {
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
         console.log(err);
-        res.status(500)
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (res, res) => {
+    User.findOne({
+        attrubutes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attrubutes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attrubutes: ['title']
+                }
+            },
+            {
+                model: Post,
+                attrubutes: ['title'],
+                through: Like,
+                as: 'liked_posts'
+            }
+        ]
     })
-})
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'Aw shucks, there\'s nobody with this name!'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
