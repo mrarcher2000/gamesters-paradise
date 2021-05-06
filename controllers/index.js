693bb60b3ce612bb4713786e11bf9ddfa3d0e76c
Duartem795 <76462661+Duartem795@users.noreply.github.com>
2021-05-05 20:47:35 -0700

    Update index.js
    added post-routes Const

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 
 //will add more routes as the files get added. please name them {name}-routes.js
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
