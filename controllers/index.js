626ed2628cf3784c5c48f2029d4abbe276e8a776
Sheldon Stevens <sgste080@yahoo.com>
2021-05-10 21:20:05 -0700

    changes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 //will add more routes as the files get added. please name them {name}-routes.js
-const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes.js');
+const userRoutes = require('./api/user-routes.js');
+const postRoutes = require('./api/post-routes.js');
 
+
+router.use('/', homeRoutes);
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 
