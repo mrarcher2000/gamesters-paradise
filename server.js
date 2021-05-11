816b0eb1ce062883296ccdc2757dc04379d6bc42
unknown <archernich@gmail.com>
2021-05-10 20:21:47 -0700

    working server

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
@@ -21,10 +25,9 @@ const sess = {
 
 app.use(session(sess));
 
-const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({ helpers });
 
+app.use(routes);
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
