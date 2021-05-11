afb3ce318a6c02e995d58bd6728d117b9ba7e0a3
Duartem795 <76462661+Duartem795@users.noreply.github.com>
2021-05-10 19:22:24 -0700

    Update index.js
    Added game and comment routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
 
 //will add more routes as the files get added. please name them {name}-routes.js
-const userRoutes = require('./user-routes.js');
+const commentRoutes = require('./comment-routes.js');
+const gameRoutes = require('./game-routes.js');
 const postRoutes = require('./post-routes.js');
+const userRoutes = require('./user-routes.js');
 
+router.use('/comments', commentRoutes);
+router.use('/games', gameRoutes);
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+
 
 module.exports = router;
