881f4df64a31558c9797df067d8e5cd67d35c49f
unknown <archernich@gmail.com>
2021-05-11 22:07:21 -0700

    added single post page

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -83,4 +83,53 @@ router.get('/edit/:id', withAuth, (req, res) => {
         });
 });
 
+
+
+// LIKE & COMMENT
+router.get('/single-post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_text',
+            'user_id'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'user_id', 'post_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if(!dbPostData) {
+                res.status(404).json({ message: 'No post found? Check yourself and try again' });
+                return;
+            }
+
+
+            const post = dbPostData.map(post => post.get({ plain: true }));
+
+            res.render('like-comment', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
