2d25898782640bdf812b165ba100118f0217ca74
Nmoss3 <75024930+Nmoss3@users.noreply.github.com>
2021-05-14 21:01:13 -0700

    double dashboard

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,142 +1,141 @@
-const router = require('express').Router();
-const sequelize = require('../../config/connection');
-const { Post, User, Comment, Like } = require('../../models');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const sequelize = require("../../config/connection");
+const { Post, User, Comment, Like } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 //gettin them users
-router.get('/', (req, res) => {
-    console.log('----------------');
-    Post.findAll({
-        attributes: [
-            'id',
-            'post_text',
-            'title',
-            'created_at'
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attribute: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
-
-router.get('/:id', (req, res) => {
-    Post.findOne({
-        where: {
-            id:req.params.id
+router.get("/", (req, res) => {
+  console.log("----------------");
+  Post.findAll({
+    attributes: ["id", "post_text", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attribute: ["username"],
         },
-        attributes : [
-            'id',
-            'post_text',
-            'title',
-            'created_at'
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attribute: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    .then(dbPostData => {
-        if(!dbPostData) {
-            res.status(404).json({ message: 'Aw schucks, this post doesn\'t exist!' });
-            return;
-        }
-        res.json(dbPostData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
-router.post('/', (req, res) => {
-    Post.create({
-        title: req.body.title,
-        post_text: req.body.post_text,
-        user_id: req.session.user_id
-    })
-        .then(dbPostData => res.json(dbPostData))
-        .catch(err => {
-            console.log(err)
-            res.status(500).json(err);
-        });
-});
-
-router.put('/like', withAuth, (req, res) => {
-    Post.like({ ...req.body, user_id: req.session.user_id }, { Like, Comment, User })
-        .then(updatedLikeData => res.json(updatedLikeData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
-
-router.put('/:id', withAuth, (req, res) => {
-    Post.update(
-        {
-            title:req.body.title
+router.get("/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "post_text", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attribute: ["username"],
         },
-        {
-            where: {
-                id: req.params.id
-            }
-        }
-    )
-    .then(dbPostData => {
-        if(!dbPostData) {
-            res.status(404).json({ message: 'Aw schucks, this post doesn\'t exist!' });
-            return;
-        }
-        res.json(dbPostData);
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res
+          .status(404)
+          .json({ message: "Aw schucks, this post doesn't exist!" });
+        return;
+      }
+      res.render("post", { dbPostData });
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
-router.delete('/:id', withAuth, (req, res) => {
-    console.log('id', req.params.id);
-    Post.destroy({
-        where: {
-            id: req.params.id
-        }
+router.post("/", (req, res) => {
+  Post.create({
+    title: req.body.title,
+    post_text: req.body.post_text,
+    user_id: req.session.user_id,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put("/like", withAuth, (req, res) => {
+  Post.like(
+    { ...req.body, user_id: req.session.user_id },
+    { Like, Comment, User }
+  )
+    .then((updatedLikeData) => res.json(updatedLikeData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", withAuth, (req, res) => {
+  Post.update(
+    {
+      title: req.body.title,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res
+          .status(404)
+          .json({ message: "Aw schucks, this post doesn't exist!" });
+        return;
+      }
+      res.json(dbPostData);
     })
-    .then(dbPostData => {
-        if(!dbPostData) {
-            res.status(404).json({ message: 'Aw schucks, this post doesn\'t exist!' });
-            return;
-        }
-        res.json(dbPostData);
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", withAuth, (req, res) => {
+  console.log("id", req.params.id);
+  Post.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res
+          .status(404)
+          .json({ message: "Aw schucks, this post doesn't exist!" });
+        return;
+      }
+      res.json(dbPostData);
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,52 +1,44 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Comment, Game, Like, Post, Review, User } = require('../models');
-
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Comment, Game, Like, Post, Review, User } = require("../models");
 
 // HOMEPAGE ROUTE
-router.get('/', (req, res) => {
-    res.render('homepage', {
-        loggedIn: req.session.loggedIn
-    });
+router.get("/", (req, res) => {
+  res.render("homepage", {
+    loggedIn: req.session.loggedIn,
+  });
 });
-
 
 // GAMES ROUTE
-router.get('/games', (req, res) => {
-    res.render('games', {
-        loggedIn: req.session.loggedIn
-    });
+router.get("/games", (req, res) => {
+  res.render("games", {
+    loggedIn: req.session.loggedIn,
+  });
 });
-
-
 
 // POST ROUTE
-router.get('/mypost', (req, res) => {
+router.get("/mypost", (req, res) => {
+  if (!req.session.loggedIn) {
+    res.redirect("/login");
+    return;
+  }
 
-    if (!req.session.loggedIn) {
-        res.redirect('/login');
-        return;
-    }
-
-
-    res.render('post', {
-        loggedIn: req.session.loggedIn
-    });
+  res.render("post", {
+    loggedIn: req.session.loggedIn,
+  });
 });
-
-
 
 // LOGIN ROUTE
-router.get('/login', (req, res) => {
-    res.render('login', {
-        loggedIn: req.session.loggedIn
-    });
+router.get("/login", (req, res) => {
+  res.render("login", {
+    loggedIn: req.session.loggedIn,
+  });
 });
 
-router.get('/dashboard', (req, res) =>{
-    res.render('dashboard', {
-        loggedIn: req.session.loggedIn
-    });
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard", {
+    loggedIn: req.session.loggedIn,
+  });
 });
 
 module.exports = router;
