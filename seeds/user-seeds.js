e3ba1be222456063e6ec55db66ee8b7dcba2bc74
Duartem795 <76462661+Duartem795@users.noreply.github.com>
2021-05-14 21:28:42 -0700

    Update user-seeds.js
    Awful names

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -7,7 +7,27 @@ const userdata = [
         username: 'epicgamer420',
         email: 'gitgud@scrub.net',
         password: 'getrekt!'
-    }
+    },
+    {
+        username: '360noscoper',
+        email: 'boom@headshot.com',
+        password: 'iamthebest'
+    },
+    {
+        username: 'Xxcooldude2007xX',
+        email: 'real@gamer.net',
+        password: 'thelegend'
+    },
+    {
+        username: '1337h4xx0r',
+        email: 'codemanipulator@gmail.com',
+        password: 'cantwinlegit'
+    },
+    {
+        username: 'RPGaddict',
+        email: 'cloud@strife.com',
+        password: 'notinterested'
+    },
 ];
 
 const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
